f88b2119f1b2051b98710519a593812fe5cd5d7e
crj0x <243283810+crj0x@users.noreply.github.com>
2025-12-17 11:02:06 +0530

    feat: implement REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  while (true)
+  {
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
 
-  std::string command;
-  std::getline(std::cin, command);
-  std::cout << command << ": command not found" << std::endl;
+    std::string command;
+    std::getline(std::cin, command);
+    std::cout << command << ": command not found" << std::endl;
+  }
 }
